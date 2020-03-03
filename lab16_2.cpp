0e5f730f6b21486203ec7bae269e6303ec631f24
sathayu2544 <59278037+sathayu2544@users.noreply.github.com>
2020-03-03 16:52:17 +0700

    Update lab16_2.cpp

--- lab16_2.cpp
+++ lab16_2.cpp
@@ -1,15 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//Write the definition of myString() here
-
-int main(){
+char myString(char *x, int y)
+{
+	char *z = new char;
+	for (int i = 0; i < y; i++)
+	{
+		z[i] = 'A' + i;
+		cout << z[i];
+	}
+}
+int main()
+{
 	char *p;
-	int N;	
+	int N;
 	cout << "N = ";
-	cin >> N;		
-	myString(p,N);
-	cout << p;	
-	delete [] p;
+	cin >> N;
+	myString(p, N);
+	cout << p;
+	delete[] p;
 	return 0;
 }
